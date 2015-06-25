61067a246f56e6ea43006d00ddb992b1c962eff7
Fabio Cavalcante <facaval@microsoft.com>
2015-06-25 10:36:18 -0700

    Original Quickstart

---
+++ fcxfquickstart/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fcxfquickstart
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services url and key.
+		public static string ApplicationURL = @"https://fcxfquickstart-code.azurewebsites.net";
+		public static string ApplicationKey = @"ISvKPmcVkKuoRRYyVYWuZFdUEGTqSw17";
+	}
+}
+
