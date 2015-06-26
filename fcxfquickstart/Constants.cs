75157ee2196e196564e77911391ac3396b3581ec
Fabio Cavalcante <facaval@microsoft.com>
2015-06-26 16:42:35 -0700

    Added support for offline sync

--- fcxfquickstart/Constants.cs
+++ fcxfquickstart/Constants.cs
@@ -5,8 +5,8 @@ namespace fcxfquickstart
 	public static class Constants
 	{
 		// Replace strings with your mobile services url and key.
-		public static string ApplicationURL = @"https://fcxfquickstart-code.azurewebsites.net";
-		public static string ApplicationKey = @"ISvKPmcVkKuoRRYyVYWuZFdUEGTqSw17";
+		public static string ApplicationURL = @"https://<myservice>.azurewebsites.net";
+		public static string ApplicationKey = @"<my application key>";
 	}
 }
 
